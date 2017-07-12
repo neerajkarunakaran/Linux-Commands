05427a49ab221661fc13966bf73b7dcdcbfb7b8a
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-07-12 16:47:12 +0530

    ls-r command

--- ls-r.c
+++ ls-r.c
@@ -4,19 +4,16 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
-#define NORMAL "\0x1b[0m"
-#define GREEN "\0x1B[32m"
-#define BLUE "\0x1b[34m"
+#define NORMAL "\x1b[0m"
+#define GREEN "\x1b[32m"
+#define BLUE "\x1b[34m"
 void read_dir_recur(char *path);
 int main(int argc, const char *argv[])
 {
-
     char *path;
     char buf[1024];
-    if (strcmp(argv[1], "-r") == 0) {
-	if ((path = getcwd(buf, 1024)) != NULL) {
-	    read_dir_recur(path);
-	}
+    if ((path = getcwd(buf, 1024)) != NULL) {
+	read_dir_recur(path);
     }
 }
 
@@ -27,16 +24,15 @@ void read_dir_recur(char *path)
     d = opendir(path);
     while ((dir = readdir(d)) != NULL) {
 	if (dir->d_type != DT_DIR) {
-	    printf("%s%s ", GREEN, dir->d_name);
+	    printf("%s%s ", NORMAL, dir->d_name);
 	} else {
 	    if (dir->d_type == DT_DIR && strcmp(dir->d_name, ".") != 0
 		&& strcmp(dir->d_name, "..") != 0) {
 		printf("%s%s ", BLUE, dir->d_name);
 		char dpath[255];
-		sprintf(dpath, "%s%s", path, dir->d_name);
-		printf("\n");
-
-
+		sprintf(dpath, "%s/%s", path, dir->d_name);
+		printf("->");
+		read_dir_recur(dpath);
 	    }
 	}
     }
